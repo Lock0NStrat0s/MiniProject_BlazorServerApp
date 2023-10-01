5e5c6eaac9d9fa6dd9cbabc697e2ef25764b7bf7
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-10-01 09:58:42 -0600

    Add project files.

---
+++ MiniProject_BlazorServer/Models/AddressModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniProject_BlazorServer.Models;
+
+public class AddressModel
+{
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string PostalCode { get; set; }
+}
---
+++ MiniProject_BlazorServer/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniProject_BlazorServer.Models;
+
+public class PersonModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsActive { get; set; }
+}
