bf64b4ab230f22cbaf2e659bab1a0883c9ce3ce5
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-10-01 10:15:24 -0600

    mini project complete.

--- MiniProject_BlazorServer/Models/AddressModel.cs
+++ MiniProject_BlazorServer/Models/AddressModel.cs
@@ -1,9 +1,20 @@
-﻿namespace MiniProject_BlazorServer.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniProject_BlazorServer.Models;
 
 public class AddressModel
 {
+    [Required]
     public string StreetAddress { get; set; }
+
+    [Required]
     public string City { get; set; }
+
+    [Required]
+    [StringLength(2)]
     public string Province { get; set; }
+
+    [Required]
+    [StringLength(6)]
     public string PostalCode { get; set; }
 }
